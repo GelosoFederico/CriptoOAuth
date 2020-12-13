2e533baffac7d5d0bebcbcf87ed6283d07dc84d1
cristin <cristinaa.kuo@gmail.com>
2020-12-13 18:10:54 -0300

    Add README and login endpoint (which does nothing)

---
+++ README.md
@@ -0,0 +1,18 @@
+# OAuth2.0
+
+# Dependencies
+python 3 (>= 3.6)
+
+# Set up
+```
+pip install -r requirements.txt
+```
+
+## Client app
+
+To run the client app, just do:git 
+```
+python client_app/app.py
+```
+
+It runs in debug mode.
--- client_app/app.py
+++ client_app/app.py
@@ -1,10 +1,16 @@
-from flask import Flask
-
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 @app.route('/hello_world')
 def hello_world():
     return 'Hello, world!'
+
+@app.route('/login')
+def login():
+    # TODO: use oauth server to redirect the URI
+    return "Here is where I give you a redirect URI but I dont know how to do it yet hah"
+
+app.run(debug=True)
 
 
