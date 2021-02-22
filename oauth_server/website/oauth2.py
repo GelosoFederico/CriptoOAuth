17d83c0f3894ad1181f11f1ce5968664fc9bf554
cristin <cristinaa.kuo@gmail.com>
2021-02-21 21:30:21 -0300

    Remove unused stuff from OAuth Server config

--- oauth_server/website/oauth2.py
+++ oauth_server/website/oauth2.py
@@ -1,20 +1,13 @@
-import os
 import datetime
 
 import jwt
-from authlib.integrations.flask_oauth2 import (
-    AuthorizationServer,
-    ResourceProtector,
-)
-from authlib.integrations.sqla_oauth2 import (
-    create_query_client_func,
-    create_revocation_endpoint,
-    create_bearer_token_validator,
-)
+from authlib.integrations.flask_oauth2 import AuthorizationServer
+from authlib.integrations.sqla_oauth2 import create_query_client_func
 from authlib.oauth2.rfc6749 import grants
 from authlib.oauth2.rfc7636 import CodeChallenge
+
 from .models import db, User
-from .models import OAuth2Client, OAuth2AuthorizationCode, OAuth2Token
+from .models import OAuth2Client, OAuth2AuthorizationCode
 
 OAUTH_PRIVATE_KEY_PATH = 'oauth_server/jwt_keys/ssh-key'
 
@@ -69,28 +62,6 @@ class AuthorizationCodeGrant(grants.AuthorizationCodeGrant):
     def authenticate_user(self, authorization_code):
         return User.query.get(authorization_code.user_id)
 
-
-class PasswordGrant(grants.ResourceOwnerPasswordCredentialsGrant):
-    def authenticate_user(self, username, password):
-        user = User.query.filter_by(username=username).first()
-        if user is not None and user.check_password(password):
-            return user
-
-
-class RefreshTokenGrant(grants.RefreshTokenGrant):
-    def authenticate_refresh_token(self, refresh_token):
-        token = OAuth2Token.query.filter_by(refresh_token=refresh_token).first()
-        if token and token.is_refresh_token_active():
-            return token
-
-    def authenticate_user(self, credential):
-        return User.query.get(credential.user_id)
-
-    def revoke_old_credential(self, credential):
-        credential.revoked = True
-        db.session.add(credential)
-        db.session.commit()
-
 def do_not_save(token, request):
     pass
 
@@ -98,26 +69,10 @@ query_client = create_query_client_func(db.session, OAuth2Client)
 
 authorization = AuthorizationServer(
     query_client=query_client,
-    save_token=do_not_save,
+    save_token=do_not_save, # Because of JWT
 )
-require_oauth = ResourceProtector()
-
 
 def config_oauth(app):
     app.config['OAUTH2_ACCESS_TOKEN_GENERATOR'] = gen_access_token
     authorization.init_app(app)
-
-    # support all grants
-    authorization.register_grant(grants.ImplicitGrant)
-    authorization.register_grant(grants.ClientCredentialsGrant)
     authorization.register_grant(AuthorizationCodeGrant, [CodeChallenge(required=True)])
-    authorization.register_grant(PasswordGrant)
-    authorization.register_grant(RefreshTokenGrant)
-
-    # support revocation
-    revocation_cls = create_revocation_endpoint(db.session, OAuth2Token)
-    authorization.register_endpoint(revocation_cls)
-
-    # protect resource
-    bearer_cls = create_bearer_token_validator(db.session, OAuth2Token)
-    require_oauth.register_token_validator(bearer_cls())
